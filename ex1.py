e8b1892c466e9b5d795cf1849a1d7068b531bba9
Gustavo Roque <154618238+gustavoroque97@users.noreply.github.com>
2024-06-25 15:16:20 -0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -2,7 +2,6 @@ import pandas            as pd
 import streamlit         as st
 import seaborn           as sns
 import matplotlib.pyplot as plt
-import openpyxl
 from PIL                 import Image
 from io                  import BytesIO
 
